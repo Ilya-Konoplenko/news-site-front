678df7542eb91d4020a86da76973f968cdb95a55
Ilya-Konoplenko <i.konoplenko@nv.dunice.net>
2022-09-21 15:02:30 +0300

    Requset's fix

--- .history/src/App_20220921144417.js
+++ .history/src/App_20220921144417.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <header className="app__header">
+      <header className="app__header">Не лезь блять!!!
       </header>
     </div>
   );
