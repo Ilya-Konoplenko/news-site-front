ac335cda3901067bf84f05e134b86278e356f1ce
Ilya-Konoplenko <i.konoplenko@nv.dunice.net>
2022-10-03 15:32:36 +0300

    Post Import fix

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,11 +1,11 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import {
+  CardActionArea,
+  Typography,
+  CardMedia, CardContent, Card,
+} from '@mui/material';
 
 function Post({ post }) {
   return (
