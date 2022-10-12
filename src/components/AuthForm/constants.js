84e8a60ee34a4394e3901641b8fe8a41a6860224
Ilya-Konoplenko <i.konoplenko@nv.dunice.net>
2022-10-12 16:37:45 +0300

    Update

--- src/components/AuthForm/constants.js
+++ src/components/AuthForm/constants.js
@@ -1,26 +1,30 @@
 import * as Yup from 'yup';
 import * as Message from './messages';
 
+const username = Yup.string()
+  .min(2, Message.TEXT_TOO_SHORT)
+  .max(20, Message.TEXT_TOO_LONG)
+  .required(Message.TEXT_REQUIRED);
+
+const password = Yup.string().required(Message.TEXT_PASSWORD_REQUIRED);
+
 const signUpValidationSchema = Yup.object().shape({
-  username: Yup.string()
-    .min(2, Message.TEXT_TOO_SHORT)
-    .max(50, Message.TEXT_TOO_LONG)
-    .required(Message.TEXT_REQUIRED),
-  password: Yup.string().required(Message.TEXT_PASSWORD_REQUIRED),
+  username,
+  password,
   email: Yup.string().email(Message.TEXT_INVALID_EMAIL).required(Message.TEXT_PASSWORD_REQUIRED),
 });
 
 const loginValidationSchema = Yup.object().shape({
-  username: Yup.string()
-    .min(2, Message.TEXT_TOO_SHORT)
-    .max(50, Message.TEXT_TOO_LONG)
-    .required(Message.TEXT_REQUIRED),
-  password: Yup.string().required(Message.TEXT_PASSWORD_REQUIRED),
+  username,
+  password,
 });
 
-const signupFileds = ['username', 'password', 'email'];
+const signupFields = ['username', 'password', 'email'];
 const loginFields = ['username', 'password'];
 
 export {
-  loginValidationSchema, signUpValidationSchema, loginFields, signupFileds,
+  loginValidationSchema,
+  signUpValidationSchema,
+  loginFields,
+  signupFields,
 };
